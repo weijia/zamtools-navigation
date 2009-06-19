402af1d165e907f2c9c8d148b3e62139b6d152cd
ZamTools <ZamTools@d247e05c-3cdc-11de-801c-c7cb65f9a556>
2009-06-19 17:51:26 +0000

    Preparing setup installer.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name = "zamtools-navigation",
+    version = "1.0",
+    packages = find_packages(),
+    author = "Ian Zamojc",
+    author_email = "zamtools@zamtools.com",
+    description = "Django application for displaying and tracking site navigation.",
+    url = "http://code.google.com/p/zamtools-navigation/",
+    include_package_data = True
+)
