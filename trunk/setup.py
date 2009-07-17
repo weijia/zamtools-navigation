b4377e7b714538b62e2af365f4d67d300f7f8447
ZamTools <ZamTools@d247e05c-3cdc-11de-801c-c7cb65f9a556>
2009-07-17 18:40:15 +0000

    Added manifest and readme.  Setup should now unpack directories to site-packages, rather than just an egg.

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 setup(
     name = "zamtools-navigation",
-    version = "1.0",
-    packages = find_packages(),
+    version = '1.0',
+    description = "Django application for displaying and tracking site navigation",
     author = "Ian Zamojc",
     author_email = "zamtools@zamtools.com",
-    description = "Django application for displaying and tracking site navigation.",
     url = "http://code.google.com/p/zamtools-navigation/",
-    include_package_data = True
+    packages = ['navigation'],
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
 )
